06bf3f4630d143e223561919bde78c3f961f9b38
vitalii <dumavb777@gmail.com>
2019-07-14 12:41:14 +0300

    Solution after corect some file

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,31 +1,30 @@
 import { getData } from '../utils';
 
-export const SHOW_POST = 'show_post';
-export const HIDE_POST = 'hide_post';
-export const CREATE_POST = 'create_post';
 export const CHANGE_NEW_POST_TITLE = 'change_new_post_title';
 export const CHANGE_NEW_POST_TEXT = 'change_new_post_text';
-export const SAVE_POST = 'save_post';
 export const CANCEL_SAVE_POST = 'cancel_save_post';
-export const CHANGE_PAGE = 'change_page';
 export const SHOW_COMMENTS = 'show_comments';
 export const HIDE_COMMENTS = 'hide_comments';
 export const SHOW_AUTHOR = 'show_author';
 export const HIDE_AUTHOR = 'hide_author';
+export const CREATE_POST = 'create_post';
+export const CHANGE_PAGE = 'change_page';
 export const FILL_DATA = 'fill_data';
+export const SAVE_POST = 'save_post';
+export const SHOW_POST = 'show_post';
+export const HIDE_POST = 'hide_post';
 
 const url = 'https://jsonplaceholder.typicode.com/';
 
 export function loadData() {
   return (dispatch) => {
-
     Promise.all([
       getData(`${url}posts`),
       getData(`${url}users`),
       getData(`${url}comments`),
     ])
       .then(([posts, users, comments]) => {
-        dispatch(fillData({posts, users, comments}));
+        dispatch(fillData({ posts, users, comments }));
       });
   };
 }
@@ -34,11 +33,10 @@ export function fillData(data) {
   return {
     type: FILL_DATA,
     payload: data,
-  }
+  };
 }
 
 export function showModalComments(postId) {
-
   return {
     type: SHOW_COMMENTS,
     payload: postId,
@@ -52,7 +50,6 @@ export function hideModalComments() {
 }
 
 export function showModalAuthor(userId) {
-
   return {
     type: SHOW_AUTHOR,
     payload: userId,
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,62 +1,62 @@
 import {
+  CANCEL_SAVE_POST, CHANGE_PAGE,
+  CHANGE_NEW_POST_TITLE,
+  CHANGE_NEW_POST_TEXT,
+  SHOW_COMMENTS,
+  HIDE_COMMENTS,
+  SHOW_AUTHOR,
+  HIDE_AUTHOR,
   CREATE_POST,
   SHOW_POST,
   HIDE_POST,
-  SHOW_COMMENTS,
-  HIDE_COMMENTS,
   FILL_DATA,
-  SHOW_AUTHOR,
-  HIDE_AUTHOR,
-  CHANGE_NEW_POST_TITLE,
-  CHANGE_NEW_POST_TEXT,
   SAVE_POST,
-  CANCEL_SAVE_POST, CHANGE_PAGE,
 } from './actions';
 
 const initialState = {
-  dataLoaded: false,
-  usersMap: null,
-  comments: null,
-  posts: null,
-  currentPostId : null,
-  currentUserId : null,
   isModalCommentsVisible: false,
   isModalAuthorVisible: false,
+  currentPostId: null,
+  currentUserId: null,
   postCreating: null,
+  dataLoaded: false,
   newPostTitle: '',
   newPostText: '',
+  usersMap: null,
+  comments: null,
   newPost: null,
   activePage: 1,
+  posts: null,
 };
 
 const actionHandlers = {
-  [FILL_DATA]:(state, action) => ({
-      ...state,
-      dataLoaded: true,
-      usersMap: action.payload.users
-          .reduce((acc, user) => ({ ...acc, [user.id]: user }), {}),
+  [FILL_DATA]: (state, action) => ({
+    ...state,
+    dataLoaded: true,
+    usersMap: action.payload.users
+      .reduce((acc, user) => ({ ...acc, [user.id]: user }), {}),
     comments: action.payload.comments,
-      posts: action.payload.posts,
+    posts: action.payload.posts,
 
   }),
   [SHOW_COMMENTS]: (state, action) => ({
     ...state,
-    isModalCommentsVisible : true,
+    isModalCommentsVisible: true,
     currentPostId: action.payload,
   }),
   [HIDE_COMMENTS]: state => ({
     ...state,
-    isModalCommentsVisible : false,
+    isModalCommentsVisible: false,
     currentPostId: null,
   }),
   [SHOW_AUTHOR]: (state, action) => ({
     ...state,
-    isModalAuthorVisible : true,
+    isModalAuthorVisible: true,
     currentUserId: action.payload,
   }),
   [HIDE_AUTHOR]: state => ({
     ...state,
-    isModalAuthorVisible : false,
+    isModalAuthorVisible: false,
     currentUserId: null,
   }),
   [SHOW_POST]: (state, action) => ({
@@ -84,7 +84,7 @@ const actionHandlers = {
   [SAVE_POST]: state => ({
     ...state,
     newPost: {
-      userId : 1,
+      userId: 1,
       title: state.newPostTitle.trim(),
       text: state.newPostText.trim(),
     },
